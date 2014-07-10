5bcb4a4b05b99d3506c8c033f8a79477256e3c27
Andrew Reitz <aj.reitz@gmail.com>
2014-07-10 14:27:46 -0500

    Continuting to work on project

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import models.BetType
+import play.Logger
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
@@ -9,24 +10,43 @@ import scala.collection.JavaConverters._
 
 object Application extends Controller {
 
-  /** Main page displaying all bets */
+  /**
+   * GET
+   * Main page displaying all bets
+   */
   def index = Action { implicit request =>
     val bets: java.util.List[models.Bet] = new Finder[Long, models.Bet](classOf[Long], classOf[models.Bet]).all()
     Ok(views.html.index(bets.asScala.toList))
   }
 
-  /** Create a new bet */
+  /**
+   * GET
+   * Create a new bet page
+   */
   def createNewBet = Action { implicit request =>
     Ok(views.html.create(""))
   }
 
-  def getBet(betId: Long) = Action { implicit request =>
+  /**
+   * GET
+   * Place a new bet page
+   */
+  def placeBet(betId: Long) = Action { implicit request =>
     val bet = new Finder[Long, models.Bet](classOf[Long], classOf[models.Bet]).byId(betId)
-    Ok(views.html.bet(bet))
+    Ok(views.html.placeBet(bet))
   }
 
-  case class PersonBet(name: String, betType: String, betId: Long)
+  /**
+   * GET
+   * Bet details page
+   */
+  def getBetDetails(betId: Long) = Action { implicit request =>
+    val bet = new Finder[Long, models.Bet](classOf[Long], classOf[models.Bet]).byId(betId)
+    Ok(views.html.betDetails(bet))
+  }
 
+  /** addPersonBet Form mapping */
+  case class PersonBet(name: String, betType: String, betId: Long)
   val personBetForm = Form(
     mapping(
       "name" -> text,
@@ -35,19 +55,23 @@ object Application extends Controller {
     )(PersonBet.apply)(PersonBet.unapply)
   )
 
+  /**
+   * POST
+   * Place a bet made by a person
+   */
   def addPersonBet = Action { implicit request =>
     val personBet = personBetForm.bindFromRequest().get
     val model = new models.PersonBet(personBet.name, BetType.fromString(personBet.betType))
     val bet = new Finder[Long, models.Bet](classOf[Long], classOf[models.Bet]).byId(personBet.betId)
-    bet.peopleBets += model
+    bet.peopleBets :+ model
     model.save()
     bet.save()
 
     Redirect(routes.Application.index)
   }
 
+  /** Add new bet form mapping */
   case class Bet(title: String, description: String)
-
   val betForm = Form(
     mapping(
       "title" -> text,
@@ -55,7 +79,10 @@ object Application extends Controller {
     )(Bet.apply)(Bet.unapply)
   )
 
-  /** Add bets */
+  /**
+   * POST
+   * Add a new bet
+   */
   def addBet = Action { implicit request =>
     val bet = betForm.bindFromRequest().get
     new models.Bet(bet.title, bet.description).save()
--- app/models/Bet.scala
+++ app/models/Bet.scala
@@ -8,5 +8,5 @@ import scala.collection.mutable.ListBuffer
 
 @Entity class Bet(val title: String, val description: String) extends Model {
   @Id var id: Long = 0
-  @OneToMany var peopleBets: ListBuffer[PersonBet] = ListBuffer()
+  @OneToMany var peopleBets: List[PersonBet] = List()
 }
